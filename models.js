7354937c6fdc352db8c381881afba814d229cb45
Kenny Lin <nilynnek@gmail.com>
2013-11-10 08:02:31 -0500

    cleaned up and added headers

--- models.js
+++ models.js
@@ -1,6 +1,9 @@
-/**
- * Models
- */
+/***************************
+ * Models                  *
+ *                         *
+ * Kenny Lin               *
+ * HackPrinceton Fall 2013 *
+ * November 10, 2013       *
+ ***************************/
 
 Messages = new Meteor.Collection('messages');
-// Users = new Meteor.Collection('users');
